7548bcd34774a8b69c0ee27dba1e35e8ee371629
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-06-28 12:05:08 -0600

    Adding new mapper for movies by id

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,5 +1,6 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 
 class MovieMapper {
   static Movie movieDBToEntity(MovieMovieDb moviedb) => Movie(
@@ -16,6 +17,27 @@ class MovieMapper {
         posterPath: (moviedb.posterPath != '')
             ? "https://image.tmdb.org/t/p/w500/${moviedb.posterPath}"
             : 'no-poster',
+        releaseDate: moviedb.releaseDate,
+        title: moviedb.title,
+        video: moviedb.video,
+        voteAverage: moviedb.voteAverage,
+        voteCount: moviedb.voteCount,
+      );
+
+  static Movie movieDetailsToEntity(MovieDetails moviedb) => Movie(
+        backdropPath: (moviedb.backdropPath != '')
+            ? "https://image.tmdb.org/t/p/w500/${moviedb.backdropPath}"
+            : 'https://www.tgv.com.my/assets/images/404/movie-poster.jpg',
+        genreIds: moviedb.genres.map((e) => e.name).toList(),
+        adult: moviedb.adult,
+        id: moviedb.id,
+        originalLanguage: moviedb.originalLanguage,
+        originalTitle: moviedb.originalTitle,
+        overview: moviedb.overview,
+        popularity: moviedb.popularity,
+        posterPath: (moviedb.posterPath != '')
+            ? "https://image.tmdb.org/t/p/w500/${moviedb.posterPath}"
+            : 'https://www.tgv.com.my/assets/images/404/movie-poster.jpg',
         releaseDate: moviedb.releaseDate,
         title: moviedb.title,
         video: moviedb.video,
