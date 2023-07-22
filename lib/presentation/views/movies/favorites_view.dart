d11f08c61c1fa6df61c2dacce1c9ae1e7b2099e6
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-07-22 10:00:13 -0600

    Adding favorite movies view

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,16 +1,70 @@
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:go_router/go_router.dart';
 
-class FavoritesView extends StatelessWidget {
+//init
+
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  bool isLoading = false;
+  bool isLastPage = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadNextPage();
+  }
+
+  void loadNextPage() async {
+    if (isLoading || isLastPage) return;
+    isLoading = true;
+
+    final movies =
+        await ref.read(favoriteMoviesProvider.notifier).loadNextpage();
+    isLoading = false;
+
+    if (movies.isEmpty) isLastPage = true;
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
+    if (favoriteMovies.isEmpty) {
+      final colors = Theme.of(context).colorScheme;
+
+      return Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(Icons.favorite_outline_sharp, size: 60, color: colors.primary),
+          Text('Oh noo!',
+              style: TextStyle(color: colors.primary, fontSize: 30)),
+          const Text('No favorite movies yet',
+              style: TextStyle(fontSize: 20, color: Colors.black45)),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () => context.go('/home/0'),
+            child: const Text('Discover movies'),
+          )
+        ],
+      ));
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites View'),
-      ),
-      body: const Center(
-        child: Text('Favoritos'),
+      body: MovieMasonry(
+        loadNextPage: loadNextPage,
+        movies: favoriteMovies,
       ),
     );
   }
